30387aec2bcdbf785554bcb90183862480f8c849
Padam Bhandari <Padam.Bhandari@datascan.com>
2016-05-05 14:29:01 -0500

    initial commit

---
+++ GenericPrincipalDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPrincipalDemo
+{
+    using System.Security.Principal;
+    using System.Threading;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetupPrincipal();
+            UsePrincipal();
+            Console.Read();
+        }
+
+        private static void SetupPrincipal()
+        {
+            //Authenticate your client
+            var id = new GenericIdentity("bob");
+
+            //fetch roles
+            var roles = new string[] {"Development","Marketing"};
+            var p = new GenericPrincipal(id,roles);
+            Thread.CurrentPrincipal = p;
+        }
+
+        private static void UsePrincipal()
+        {
+            var p = Thread.CurrentPrincipal;
+            Console.WriteLine("Identity is: {0}",p.Identity.Name);
+            Console.WriteLine("Role is: {0}",p.IsInRole("Development"));
+
+        }
+    }
+}
---
+++ SAMLDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAMLDemo
+{
+    using System.Security.Principal;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = WindowsIdentity.GetCurrent();
+            Console.WriteLine(id.Name);
+
+            var account = new NTAccount(id.Name);
+            var sid = account.Translate(typeof(SecurityIdentifier));
+            Console.WriteLine(sid.Value);
+
+            foreach (var group in id.Groups.Translate(typeof(NTAccount)))
+            {
+                //Console.WriteLine(group.Value);
+               // Console.WriteLine(group);
+            }
+
+            var principal = new WindowsPrincipal(id);
+            Console.WriteLine(principal.IsInRole("Builtin\\Users"));
+           var localAdmins = new SecurityIdentifier(WellKnownSidType.BuiltinAdministratorsSid,null);
+            Console.WriteLine(principal.IsInRole(localAdmins));
+
+            var domainAdmin = new SecurityIdentifier(WellKnownSidType.BuiltinAdministratorsSid, id.User.AccountDomainSid);
+            Console.WriteLine("Domain Admin{0}",domainAdmin);
+
+           
+
+
+
+            Console.Read();
+        }
+    }
+}
